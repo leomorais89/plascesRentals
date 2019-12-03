bfa56b008524ee56cf955094bcc4b13d176766d8
Leonardo Morais <leonardomorais89@yahoo.com.br>
2019-12-03 12:31:13 -0300

    Sistema criado

---
+++ src/main/java/com/places/placesRentals/PlacesRentalsApplication.java
@@ -0,0 +1,13 @@
+package com.places.placesRentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacesRentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacesRentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/places/placesRentals/PlacesRentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.places.placesRentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacesRentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
