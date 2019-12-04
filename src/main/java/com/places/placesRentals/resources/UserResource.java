536d96be6e49ed9d53dac30545c2260702756265
Leonardo Morais <leonardomorais89@yahoo.com.br>
2019-12-04 11:52:17 -0300

    CRUD do usuario funcionando

--- src/main/java/com/places/placesRentals/resources/UserResource.java
+++ src/main/java/com/places/placesRentals/resources/UserResource.java
@@ -1,12 +1,19 @@
 package com.places.placesRentals.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.places.placesRentals.documents.User;
 import com.places.placesRentals.services.UserServices;
@@ -22,5 +29,30 @@ public class UserResource {
 	public ResponseEntity<List<User>> findAll(){
 		List<User> users = service.findAll();
 		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user){
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable String id, @RequestBody User user){
+		user = service.update(id, user);
+		return ResponseEntity.ok().body(user);
 	}
 }
--- src/main/java/com/places/placesRentals/services/UserServices.java
+++ src/main/java/com/places/placesRentals/services/UserServices.java
@@ -17,4 +17,30 @@ public class UserServices {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		return repo.findById(id).get();
+	}
+	
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+	
+	public void delete(String id) {
+		repo.deleteById(id);
+	}
+	
+	public User update(String id, User user) {
+		User newUser = findById(id);
+		newUser.setName(user.getName());
+		newUser.setBirthDate(user.getBirthDate());
+		newUser.setTelephone(user.getTelephone());
+		newUser.setCellphone(user.getCellphone());
+		newUser.setEmail(user.getEmail());
+		newUser.setAddress(user.getAddress());
+		newUser.setState(user.getState());
+		newUser.setCity(user.getCity());
+		newUser.setNeighborhood(user.getNeighborhood());
+		return repo.save(newUser);
+	}
 }
